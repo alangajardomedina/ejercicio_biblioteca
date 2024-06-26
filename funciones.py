8632805b39668f3ca681f916748b48fdfee15a67
Jose <jo.penalozas@duocuc.cl>
2024-06-26 09:59:50 -0400

    listo

--- funciones.py
+++ funciones.py
@@ -1,14 +1,68 @@
 libros = []
 #funciones del ejercicio
 def agregar_libro():
-    pass
+    print("Agregar libro")
+    titulo=input("ingrese nombre: ")
+    autor=input("ingrese autor: ")
+    anio=int(input("ingrese año: "))
+    genero=input("ingrese genero: ")
+    libro={"titulo":titulo,
+            "autor":autor,
+            "anio":anio,
+            "genero":genero}
+    libros.append(libro)
+    print("libro agregado")
+
 def mostrar_libros():
-    pass
+    if not libros:
+        print("NO existe libros")
+    else:
+        print("\t lista de libros")
+        for li in libros:
+            print("Titulo",li["titulo"])
+            print("Autor",li["autor"])
+            print("AÑO",li["anio"])
+            print("Genero",li["genero"])
+
 def buscar_libro():
-    pass
+    if not libros:
+        print("NO existe libros")
+    else:
+
+        print("\t Buscar libros")
+        titulo_buscar=input("ingrese titulo que desea: ")
+        for li in libros:
+            if titulo_buscar.lower()==li["titulo"].lower():
+                print("Titulo",li["titulo"])
+                print("Autor",li["autor"])
+                print("AÑO",li["anio"])
+                print("Genero",li["genero"])
+                break
+
 def modificar_libro():
-    pass
+    if not libros:
+        print("NO existe libros")
+    else:
+
+        print("\t modificar libros")
+        titulo_mod=input("ingrese titulo que desea: ")
+        for li in libros:
+            if titulo_mod.lower()==li["titulo"].lower():
+                li["autor"]=input("ingrese nuevo autor: ")
+                li["anio"]=int(input("ingrese nuevo año: "))
+                li["genero"]=input("ingrese nuevo genero: ")
+                print("libro modificado")
+                break
+
 def exportar_json():
-    pass
+    if not libros:
+        print("NO existen libros")
+    else:
+        nombre_archivo=input("ingrese nombre archivo: ")+".json"
+        import json
+        with open (nombre_archivo,"w") as archivo:
+            json.dump(libros,archivo,indent=2)
+        print("archivo creado")
 def salir():
-    pass
+    print("Adios")
+    exit()
--- principal.py
+++ principal.py
@@ -13,15 +13,15 @@ while True:
     opc = int(input("Ingrese opción: "))
     os.system('cls')
     if opc==1:
-        pass
+        agregar_libro()
     elif opc==2:
-        pass
+        mostrar_libros()
     elif opc==3:
-        pass
+        buscar_libro()
     elif opc==4:
-        pass
+        modificar_libro()
     elif opc==5:
-        pass
+        exportar_json()
     else:
-        pass
-    time.sleep(3)
+        salir()
+    
