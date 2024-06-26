eefcd0832105f1dfd6d484f6d7346b4676faab08
javier rojas <javie.rojasc@duouc.cl>
2024-06-26 09:57:24 -0400

    falta validar

--- funciones.py
+++ funciones.py
@@ -1,14 +1,63 @@
 libros = []
 #funciones del ejercicio
 def agregar_libro():
-    pass
+    print("agregar libro")
+    titulo = input("ingrese titulo:")
+    autor = input("ingrese autor:")
+    anio = input("ingrese año de publicacion :")
+    genero = input("ingrese genero:")
+    libro = {"titulo":titulo, "autor": autor, "anio":anio, "genero":genero}
+    libros.append(libro)
 def mostrar_libros():
-    pass
+    if not libros:
+        print("no existen libros,porfavor agrege")
+    else:
+        print("\tlista de libros")
+        for li in libros:
+            print("titulo", li['titulo'])
+            print("autor", li['autor'])
+            print("año", li['anio'])
+            print("genero", li['genero'])
+            print()
 def buscar_libro():
-    pass
+    if not libros:
+        print("no existen libros,porfavor agrege")
+    else:
+        print("buscar libro")
+        titulo_buscar=input("ingrese titulo del libro que desa: ")
+        for li in libros:
+            if titulo_buscar.lower()==li["titulo"].lower():
+                print("titulo", li['titulo'])
+                print("autor", li['autor'])
+                print("año", li['anio'])
+                print("genero", li['genero'])
+                return
+        print(f"el titulo no existe {titulo_buscar}")
 def modificar_libro():
-    pass
+    if not libros:
+        print("no existen libros,porfavor agrege")
+    else:
+        print("buscar libro")
+        modificar_titulo=input("ingrese titulo del libro que desa: ")
+        for li in libros:
+            if modificar_titulo.lower()==li["titulo"].lower():
+                print("titulo", li['titulo'])
+                print("autor", li['autor'])
+                print("año", li['anio'])
+                print("genero", li['genero'])
+                return
+        print(f"el titulo no existe{modificar_libro}")
 def exportar_json():
-    pass
+    if not libros:
+        print("no existen libros,porfavor agrege")
+    else:
+        nombre_archivo=input("ingrese el nombre del archivo")+".jsno"
+        import json
+        with open(nombre_archivo, "W")as archivo:
+            json.dump(libros,archivo,indent=2)
+        print("archivo creado con éxito")
+
+
 def salir():
-    pass
+    print("gracias, adios")
+    exit()
